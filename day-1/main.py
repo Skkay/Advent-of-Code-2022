0b52f0665105317d9aebcf75d198b9b7ce1a876d
Skkay <skkay.thomas@gmail.com>
2022-12-01 18:57:06 +0100

    Day 1 - Part 2

--- day-1/main.py
+++ day-1/main.py
@@ -1,15 +1,16 @@
 with open('day-1/inputs.txt', 'r') as f:
     inputs = f.readlines()
 
-max = 0
+max = []
 n = 0
 for line in inputs:
     if line == '\n':
-        if n > max:
-            max = n
+        max.append(n)
         n = 0
         continue
 
     n += int(line)
 
-print(max)
+max.sort(reverse=True)
+
+print(sum(max[:3]))
