978979ce7c53757d7515dc47ab7aef657822f4fd
Skkay <skkay.thomas@gmail.com>
2022-12-02 19:21:02 +0100

    Day 2 - Part 2

--- day-2/main.py
+++ day-2/main.py
@@ -1,20 +1,18 @@
 with open('day-2/inputs.txt', 'r') as f:
     inputs = f.readlines()
 
+win_pts = { 'A': 2, 'B': 3, 'C': 1 }
+draw_pts = { 'A': 1, 'B': 2, 'C': 3 }
+lose_pts = { 'A': 3, 'B': 1, 'C': 2 }
 score = 0
 for line in inputs:
     match = line.strip().split(' ')
-    if (match[0] == 'A' and match[1] == 'Y') or (match[0] == 'B' and match[1] == 'Z') or (match[0] == 'C' and match[1] == 'X'):
-        score += 6
 
-    elif (match[0] == 'A' and match[1] == 'X') or (match[0] == 'B' and match[1] == 'Y') or (match[0] == 'C' and match[1] == 'Z'):
-        score += 3
-
-    if match[1] == 'X':
-        score += 1
+    if match[1] == 'Z':
+        score += 6 + win_pts[match[0]]
     elif match[1] == 'Y':
-        score += 2
-    elif match[1] == 'Z':
-        score += 3
+        score += 3 + draw_pts[match[0]]
+    else:
+        score += lose_pts[match[0]]
 
 print(score)
