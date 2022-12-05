9d9eeec4c0e7a0f9114820833bbce003518c2f44
Skkay <skkay.thomas@gmail.com>
2022-12-05 19:34:58 +0100

    Day 4 - Part 2

--- day-4/main.py
+++ day-4/main.py
@@ -1,4 +1,4 @@
-with open('./inputs.txt') as f:
+with open('./day-4/inputs.txt') as f:
     inputs = f.readlines()
 
 sum = 0
@@ -12,7 +12,7 @@ for line in inputs:
     pair_2_from = int(pair[1].split('-')[0])
     pair_2_to = int(pair[1].split('-')[1])
 
-    if (pair_2_from >= pair_1_from and pair_2_to <= pair_1_to) or (pair_1_from >= pair_2_from and pair_1_to <= pair_2_to):
+    if (pair_2_from >= pair_1_from and pair_2_to <= pair_1_to) or (pair_1_from >= pair_2_from and pair_1_to <= pair_2_to) or (pair_2_from <= pair_1_to and pair_2_to >= pair_1_to) or (pair_2_to >= pair_1_from and pair_2_from <= pair_1_to):
         sum += 1
 
 print(sum)
